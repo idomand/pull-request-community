1b1f691d5e95a689ed1e5923598f8522a2e03e40
ido mandelman <idomand@gmail.com>
2021-05-20 11:43:37 +0300

    filter Poeple based on 1 social picked - working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { GetStaticProps } from 'next';
 import Description from '../components/description/Description';
@@ -14,24 +14,54 @@ interface IHomeProps {
 
 export default function Home({ people }: IHomeProps) {
   const [peopleToDisplay, setPeopleToDisplay] = useState(people);
-
+  /**
+ //* searchPrams ==>  searchPrams[0] = people search('string'). searchPrams[1] = social search(string[]). 
+ */
   const [searchPrams, setSearchPrams] = useState([]);
   const callbackFunc = (callbackResult) => {
-    console.log(`callbackResult`, callbackResult);
+    console.log(`searchPrams`, callbackResult);
     setSearchPrams(callbackResult);
-    console.log(`searchPrams`, searchPrams);
   };
+
+  //!========
+  useEffect(() => {
+    let newPeopleArray = [];
+    let count = 0;
+    if (searchPrams[1]) {
+      console.log(`searchPrams[1]`, searchPrams[1]);
+      if (searchPrams[1].length > 0) {
+        for (let i = 0; i < people.length; i++) {
+          for (let h = 0; h < searchPrams[1].length; h++) {
+            if (people[i][searchPrams[1]]) {
+              console.log(`people[i][searchPrams[1]]`, people[i][searchPrams[1]]);
+              newPeopleArray.push(people[i]);
+              count++;
+            }
+          }
+        }
+        console.log(`newPeopleArray`, newPeopleArray);
+        console.log(`count`, count);
+        setPeopleToDisplay(newPeopleArray);
+      }
+    }
+  }, [searchPrams]);
 
   return (
     <Layout>
       <div className={styles.container}>
-        <SearchBar callbackFunc={callbackFunc} />
         <div>{searchPrams[0]}</div>
-        <div>{searchPrams[1] && searchPrams[1][0]}</div>
+        <div>
+          {searchPrams[1] &&
+            searchPrams[1].map((element) => {
+              return <div>{element}</div>;
+            })[0]}
+        </div>
+
+        <SearchBar callbackFunc={callbackFunc} />
 
         <Description />
         <div className={styles.cards__wrapper}>
-          {people.map((person, i) => (
+          {peopleToDisplay.map((person, i) => (
             <PersonCard key={person.name + i} person={person} />
           ))}
         </div>
